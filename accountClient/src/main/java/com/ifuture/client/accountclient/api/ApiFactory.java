d94eac1642fedc8299168a26a5da4e9541eebdfa
KCherd <krisacherd@gmail.com>
2022-05-25 21:55:01 +0300

    fixed ApiFactory

--- accountClient/src/main/java/com/ifuture/client/accountclient/api/ApiFactory.java
+++ accountClient/src/main/java/com/ifuture/client/accountclient/api/ApiFactory.java
@@ -1,11 +1,8 @@
 package com.ifuture.client.accountclient.api;
 
-//Импортируем необходимые классы
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-//Объявляем фабрику - только статичные поля и методы
 public class ApiFactory {
 
     private static final String ROOT_URL = "http://localhost:8080";
